876044847480f829b2c35fbd8f710eae2abca355
shahla-kinattingal <shahalaishaque62@gmail.com>
2023-05-19 22:22:21 -0700

    iocodtext

---
+++ script.js
@@ -0,0 +1,157 @@
+function mouseOver(element){
+    element.style.color='yellow'
+}
+function mouseOut(element){
+    element.style.color='blueviolet'
+}
+
+
+ // Get the link element
+ var link = document.querySelector('.link');
+
+ // Add event listeners
+ link.addEventListener('mouseover', function() {
+   // Change the link color on mouse over
+   this.style.color = 'blue';
+ });
+
+ link.addEventListener('mouseout', function() {
+   // Restore the link color when the mouse moves out
+   this.style.color = 'white';
+ });
+
+
+// Get the element
+const element = document.getElementById('myElement');
+
+// Add event listener for mouseover
+element.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element.innerText = 'WEB DEVELOPMENT';
+});
+
+// element1
+
+// Get the element
+const element1 = document.getElementById('myElement1');
+
+// Add event listener for mouseover
+element1.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element1.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element1.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element1.innerText = 'MOBILE APP DEVELOPMENT';
+});
+
+// element2
+
+// Get the element
+const element2 = document.getElementById('myElement2');
+
+// Add event listener for mouseover
+element2.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element2.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element2.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element2.innerText = 'UI/UX DESIGN';
+});
+
+// element3
+
+// Get the element
+const element3 = document.getElementById('myElement3');
+
+// Add event listener for mouseover
+element3.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element3.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element3.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element3.innerText = 'CONTENT STRATEGY';
+});
+// element4
+
+// Get the element
+const element4 = document.getElementById('myElement4');
+
+// Add event listener for mouseover
+element4.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element4.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element4.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element4.innerText = 'INVESTMENT MANAGMENT PORTAL';
+});
+
+// element5
+
+// Get the element
+const element5 = document.getElementById('myElement5');
+
+// Add event listener for mouseover
+element5.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element5.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element5.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element5.innerText = 'CYPTO CURRENCY & BLOCKCHAIN APPLICATION';
+});
+
+// element6
+
+// Get the element
+const element6 = document.getElementById('myElement6');
+
+// Add event listener for mouseover
+element6.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element6.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element6.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element6.innerText = 'CROWD FUNDING SOFTWARE';
+});
+
+// element7
+
+// Get the element
+const element7 = document.getElementById('myElement7');
+
+// Add event listener for mouseover
+element7.addEventListener('mouseover', function() {
+  // Change the text on mouse hover
+  element7.innerText = 'we create hybrid mobile aplication using flutter react native. Our ready to use or custom made apps can be integrated with all our business solution.';
+});
+
+// Add event listener for mouseout (to revert back to original text)
+element7.addEventListener('mouseout', function() {
+  // Change the text back on mouseout
+  element7.innerText = 'MERCHANT CASH ADVANCE PORTAL';
+});
+
+
